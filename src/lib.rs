2439f8ff8e596ee3cd9d85c6d47abbc0a7ab6e74
Alex Severin <severinalexeyv@gmail.com>
2024-05-29 01:11:36 +0300

    add T bounds and rm sync
    Signed-off-by: Luca Cominardi <luca.cominardi@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -53,8 +53,7 @@ pub struct RingBuffer<T, const N: usize> {
     idx_w: CachePadded<AtomicUsize>,
 }
 
-unsafe impl<T, const N: usize> Send for RingBuffer<T, N> {}
-unsafe impl<T, const N: usize> Sync for RingBuffer<T, N> {}
+unsafe impl<T: Send, const N: usize> Send for RingBuffer<T, N> {}
 
 impl<T, const N: usize> RingBuffer<T, N> {
     #[allow(clippy::new_ret_no_self)]
